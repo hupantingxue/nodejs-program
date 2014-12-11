fe41eae75e06dd6cfe5d94bd67431a8f7dd4d3dc
hupantingxue <hupantingxue@126.com>
2014-12-11 11:20:41 +0800

    Event listener more than one;

--- event/event.js
+++ event/event.js
@@ -1,10 +1,14 @@
 var EventEmitter = require('events').EventEmitter;
 var event_obj = new EventEmitter();
 
-event_obj.on("some_event", function(){
-    console.log("some_event occured!");
+event_obj.on("some_event", function(arg1, arg2){
+    console.log("listen1 " + arg1 + arg2);
+});
+
+event_obj.on("some_event", function(arg1, arg2){
+    console.log("listen2 " + arg1 + arg2);
 });
 
 setTimeout(function() {
-    event_obj.emit("some_event");
+    event_obj.emit("some_event", "hupantingxue", " need money");
 }, 1000);
